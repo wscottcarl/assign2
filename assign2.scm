cd69c8c4a353d26ca2c4be9ff270a1febd32a9e2
CS Lounge <null>
2016-10-26 00:01:42 -0500

    added signature for first problem

---
+++ assign2.scm
@@ -0,0 +1,17 @@
+
+
+(define (author)
+   (println "AUTHOR: William Scott Carl wscarl@crimson.ua.edu")
+   )
+   
+   
+(define (iterator # $x items $body)
+   (map (eval (lambda CONSTRUCTEDLISTHERE)) items)
+   )
+
+
+
+
+(define (run1)
+   (exprTest ())
+   )
